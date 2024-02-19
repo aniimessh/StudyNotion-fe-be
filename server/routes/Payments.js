6261446224917447780352c506d6bee571922eba
aniimessh <animeshgupta2.002@gmail.com>
2024-02-19 20:15:28 +0530

    feat: new paymnt controller implemented

--- server/routes/Payments.js
+++ server/routes/Payments.js
@@ -2,9 +2,9 @@
 const express = require("express")
 const router = express.Router()
 
-const { capturePayment, verifySignature } = require("../controllers/Payments")
+const { capturePayment, verifyPayment } = require("../controllers/Payments")
 const { auth, isInstructor, isStudent, isAdmin } = require("../middlewares/auth")
 router.post("/capturePayment", auth, isStudent, capturePayment)
-router.post("/verifySignature", verifySignature)
+router.post("/verifySignature",auth, isStudent, verifyPayment)
 
 module.exports = router
