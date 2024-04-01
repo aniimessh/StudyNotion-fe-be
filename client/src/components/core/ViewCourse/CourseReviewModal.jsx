61aca124e4d3e93cd8c3c978ff230c4c2ffb73b4
aniimessh <animeshgupta2.002@gmail.com>
2024-04-01 23:14:02 +0530

    feat: revirew modal developed

--- client/src/components/core/ViewCourse/CourseReviewModal.jsx
+++ client/src/components/core/ViewCourse/CourseReviewModal.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { IoMdClose } from "react-icons/io";
 import { useSelector } from "react-redux";
+import { useForm } from "react-hook-form";
+import ReactStars from "react-rating-stars-component";
 
 const CourseReviewModal = ({ setReviewModal }) => {
   const { user } = useSelector((state) => state.profile);
   const { token } = useSelector((state) => state.auth);
+
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  useEffect(() => {
+    setValue("courseExperience", "");
+    setValue("courseRating", 0);
+  }, []);
+
+  const onSubmit = () => {};
+
+  const ratingChange = () => {};
   return (
     <div>
       <div>
@@ -15,10 +33,32 @@ const CourseReviewModal = ({ setReviewModal }) => {
           </button>
         </div>
         <div>
+          <div>
+            <img
+              src={user?.image}
+              alt="user-image"
+              className="aspect-square rounded-full w-[50px] object-cover"
+            />
             <div>
-                <img src={user?.image} alt="" />
-            {/* 1:12:00 */}
+              <p>
+                {user?.firstName}
+                {user?.lastName}
+              </p>
+              <p>Posting Publicy</p>
             </div>
+          </div>
+
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="mt-6 flex flex-col"
+          >
+            <ReactStars
+            count={5}
+            onChange={ratingChange}
+            size={24}
+            activeColor="#ffd700"
+            />
+          </form>
         </div>
       </div>
     </div>
